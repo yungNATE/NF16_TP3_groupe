d08cb8afe7f141aa611ff885d3a8cf256fe05d1f
alex16-1 <a.ghitu@yahoo.com>
2023-04-08 12:19:44 +0200

    Ajout et affichage des rayons

--- main.c
+++ main.c
@@ -1,64 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tp3.h"
+#include "tp3.c"
 
-int main()
+int main(void)
 {
     
-    T_Magasin *mon_magasin = NULL;
+    // T_Magasin *mon_magasin = NULL;
 
-    // ============= MENU UTILISATEUR ============= */
-    char choix = '0';
-    while (choix != '9') {
-        printf("\n======================================");
-        printf("\n1. Creer un magasin");
-        printf("\n2. Ajouter un rayon au magasin");
-        printf("\n3. Ajouter un produit dans un rayon");
-        printf("\n4. Afficher les rayons du magasin");
-        printf("\n5. Afficher les produits d'un rayon");
-        printf("\n6. Supprimer un produit");
-        printf("\n7. Supprimer un rayon");
-        printf("\n8. Rechercher un produit par prix");
-        printf("\n9. Quitter");
-        printf("\n======================================");
-        printf("\n   Votre choix ? ");
-        choix = getchar();
-        viderBuffer(); // supprimer de l'entr�e standard le retour charriot et les �ventuels caract�res suppl�mentaires tap�s par l'utilisateur
+    // // ============= MENU UTILISATEUR ============= */
+    // char choix = '0';
+    // while (choix != '9') {
+    //     printf("\n======================================");
+    //     printf("\n1. Creer un magasin");
+    //     printf("\n2. Ajouter un rayon au magasin");
+    //     printf("\n3. Ajouter un produit dans un rayon");
+    //     printf("\n4. Afficher les rayons du magasin");
+    //     printf("\n5. Afficher les produits d'un rayon");
+    //     printf("\n6. Supprimer un produit");
+    //     printf("\n7. Supprimer un rayon");
+    //     printf("\n8. Rechercher un produit par prix");
+    //     printf("\n9. Quitter");
+    //     printf("\n======================================");
+    //     printf("\n   Votre choix ? ");
+    //     choix = getchar();
+    //     viderBuffer(); // supprimer de l'entr�e standard le retour charriot et les �ventuels caract�res suppl�mentaires tap�s par l'utilisateur
 
-        switch (choix) {
-            case '1' :
-                break;
+    //     switch (choix) {
+    //         case '1' :
+    //             break;
 
-            case '2' :
-                break;
+    //         case '2' :
+    //             break;
 
-            case '3' :
-                break;
+    //         case '3' :
+    //             break;
 
-            case '4' :
-                break;
+    //         case '4' :
+    //             break;
 
-            case '5' :
-                break;
+    //         case '5' :
+    //             break;
 
-            case '6' :
-                break;
+    //         case '6' :
+    //             break;
 
-            case '7' :
-               break;
+    //         case '7' :
+    //            break;
 
-            case '8' :
-               break;
+    //         case '8' :
+    //            break;
 
-            case '9' :
-                printf("\n======== PROGRAMME TERMINE ========\n");
-                break;
+    //         case '9' :
+    //             printf("\n======== PROGRAMME TERMINE ========\n");
+    //             break;
 
-            default :
-                printf("\n\nERREUR : votre choix n'est valide ! ");
-        }
-        printf("\n\n\n");
-    }
+    //         default :
+    //             printf("\n\nERREUR : votre choix n'est valide ! ");
+    //     }
+    //     printf("\n\n\n");
+    // }
 
     return 0;
 }
